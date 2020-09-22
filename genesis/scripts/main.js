426bee9605455a415f4ce85e8013ac04644add8c
txvvgnx <54497983+txvvgnx@users.noreply.github.com>
2020-09-22 17:59:03 +0800

    Async await baybee

--- genesis/scripts/main.js
+++ genesis/scripts/main.js
@@ -1,18 +1,21 @@
 var modal = document.getElementById('id01');
 var modalContent = document.getElementById('loginform');
+var modalContainer = document.getElementById('modalcontainer');
 var loginButton = document.getElementById('ls');
 loginButton.onclick = function(e) {
-    modal.style.display = 'block';
+    modal.style.display = 'flex';
     document.body.style.overflow = 'hidden';
     document.body.scroll = 'no';
 }
 window.onclick = async function(e) {
     if(e.target == modal) {
         modalContent.classList.add('unzoom');
-        await new Promise(r => setTimeout(r, 450));
+        modalContainer.classList.add('unzoom');
+        await new Promise(r => setTimeout(r, 280));
         modal.style.display = 'none';
         document.body.style.overflow = 'auto';
         document.body.scroll = 'yes';
+        modalContent.classList.remove('unzoom');
         modalContent.classList.remove('unzoom');
     }
 }
